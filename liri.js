0ffa927629a85c86ce33e2ba8d6bd077e9165b77
Andrew <andrewmd4774@gmail.com>
2018-07-22 11:21:49 -0400

    going to workout

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var Twitter = require("twitter");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 if (process.argv[2] === "my-tweets") {
